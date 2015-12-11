1d0e61698616a64fcae471c6c72a9471d0bda3ce
Marianna Vladimirova <vvladimirova93@gmail.com>
2015-12-11 21:08:39 +0200

    Login form  - add angular2 to package.json

--- src/components/login/loginForm.tsx
+++ src/components/login/loginForm.tsx
@@ -12,15 +12,22 @@ import {Input, ButtonInput} from "react-bootstrap";
     }
     
     export class LoginForm extends React.Component<LoginProps, LoginState>{
+        logIn = () => {
+            let password = ReactDOM.findDOMNode<HTMLInputElement>(this.refs["password"]).value;
+            console.log(password);
+        }
+
         render() {
             return (
                 <form>
-                    <Input type="text" label="User Name" placeholder="Enter user name"/>
-                    <Input type="password" label="Password" placeholder="Enter password"/>
-                    <ButtonInput type="submit" value="Submit"/>
+                    <Input type="text" ref="userName" label="User Name" placeholder="Enter user name"/>
+                    <Input type="password" label="Password" placeholder="Enter password" ref="password"/>
+                    <ButtonInput value="Submit" onClick={this.logIn}/>
                 </form>
             )
         }
+        
+        
     }
     // const inputTypeInstance = (
     //     <form>
